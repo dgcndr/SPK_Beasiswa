7c10c78d253a83cbd64aa77da59038d19f52df61
Degi Candra Kamarullah <53246538+dgcndr@users.noreply.github.com>
2020-01-15 15:08:48 +0700

    Add files via upload

---
+++ page/home.php
@@ -0,0 +1,10 @@
+<body bgcolor="Pink">
+<h1><p align="left">
+<font color="maroon">
+   	Selamat Datang di Sistem Pendukung Keputusan <br>
+	Penentuan Penerima Beasiswa<br>
+	Menggunakan Metode Simple Additive Weighting (SAW)
+</h1> 
+</b>
+</p>
+</font>
